645b76359babd10479c007bdb8aaef75c89e7862
zkname <zhangkai@foxmail.com>
2017-08-21 12:30:47 +0800

    1

--- Zkname/src/main/java/com/zkname/DatabaseConfiguration.java
+++ Zkname/src/main/java/com/zkname/DatabaseConfiguration.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 import org.jdbcdslog.ConnectionPoolDataSourceProxy;
