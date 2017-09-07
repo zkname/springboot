c8f2c32c5a6b4671b3c92318ebf8a42c8b332c72
zkname <zhangkai@foxmail.com>
2017-09-07 10:58:02 +0800

    1

--- credit-card-web/src/main/java/com/zkname/credit/card/controller/admin/card/CcardInfoController.java
+++ credit-card-web/src/main/java/com/zkname/credit/card/controller/admin/card/CcardInfoController.java
@@ -108,6 +108,7 @@ public class CcardInfoController extends BaseController{
 		if(LoginUser.getUser().getId().longValue()!=entityUpdate.getCreatorId().longValue()){
 			throw new ActionException("参数错误！");
 		}
+		entity.setJobDate(entityUpdate.getJobDate());
 		//获取参数实体，操作更新实体，不拷贝字段
 		BeanUtils.copyProperties(entity,entityUpdate,new String[]{"id","createTime","updateTime","deleStatus","creatorId","jobDate"});
 		entityUpdate.setNextUp(entity.getNextUp());
