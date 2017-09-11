38931c07672fa8cba1d4a7bb416aa0756df3df5e
zkname <zhangkai@foxmail.com>
2017-09-11 17:34:27 +0800

    1

--- credit-card-web/src/main/java/com/zkname/credit/card/util/task/CinvitationCodeTask.java
+++ credit-card-web/src/main/java/com/zkname/credit/card/util/task/CinvitationCodeTask.java
@@ -1,31 +1,18 @@
 package com.zkname.credit.card.util.task;
 
-import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
-import org.apache.commons.lang3.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import com.zkname.core.util.CompuUtils;
-import com.zkname.core.util.DateUtil;
-import com.zkname.core.util.ParamType;
-import com.zkname.credit.card.entity.CcardInfo;
-import com.zkname.credit.card.entity.CcardRange;
 import com.zkname.credit.card.entity.CinvitationCodeBatch;
-import com.zkname.credit.card.service.CcardInfoService;
-import com.zkname.credit.card.service.CcardJobService;
-import com.zkname.credit.card.service.CcardRangeService;
 import com.zkname.credit.card.service.CinvitationCodeBatchService;
-import com.zkname.credit.card.service.CinvitationCodeService;
 
 /**
  * 生成注册码
