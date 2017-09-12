ca8d125e67cdc9d698b2b9ac6663314eb7cc1716
zkname <zhangkai@foxmail.com>
2017-09-12 15:20:42 +0800

    1

--- credit-card-web/src/main/java/com/zkname/credit/card/controller/admin/card/CcardCalendarController.java
+++ credit-card-web/src/main/java/com/zkname/credit/card/controller/admin/card/CcardCalendarController.java
@@ -1,6 +1,8 @@
 package com.zkname.credit.card.controller.admin.card;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.google.common.collect.Maps;
 import com.zkname.core.controller.BaseController;
+import com.zkname.core.util.CompuUtils;
 import com.zkname.core.util.DateUtil;
 import com.zkname.core.util.jackson.JsonUtil;
 import com.zkname.credit.card.service.CcardJobService;
@@ -43,7 +47,36 @@ public class CcardCalendarController extends BaseController{
 	@ResponseBody
 	@RequestMapping(value = "/data", produces = "application/text; charset=utf-8" ,method = RequestMethod.GET)
 	public String data(String start,String end,HttpServletRequest request,HttpServletResponse response) {
-		return JsonUtil.toJSONString(service.getDAO().findUserJob(LoginUser.getUser().getId(),DateUtil.Str2Date(start),DateUtil.Str2Date(end)));
+		List<Map<String,Object>> list=service.getDAO().findUserJob(LoginUser.getUser().getId(),DateUtil.Str2Date(start),DateUtil.Str2Date(end));
+		Map<String,Map<String,Object>> map=Maps.newHashMap();
+		for(Map<String,Object> obj:list){
+			int status=(Integer) obj.get("status");
+			String d=(String) obj.get("start");
+			double money=(Double)obj.get("money");
+			double fee=(Double)obj.get("fee");
+			obj.remove("money");
+			obj.remove("fee");
+			Map<String,Object> map2=null;
+			if(!map.containsKey(d)){
+				map2=Maps.newHashMap();
+				map2.put("start", d);
+				map2.put("status", 2);
+				map2.put("money", 0D);
+				map2.put("nomoney", 0D);
+				map2.put("fee", 0D);
+				map.put(d, map2);
+			}else{
+				map2=map.get(d);
+			}
+			if(status==1){
+				map2.put("money", CompuUtils.add(money,(Double) map2.get("money"),2));
+				map2.put("fee", CompuUtils.add(fee,(Double) map2.get("fee"),2));
+			}else{
+				map2.put("nomoney", CompuUtils.add(money,(Double) map2.get("nomoney"),2));
+			}
+		}
+		list.addAll(map.values());
+		return JsonUtil.toJSONString(list);
 	}
 	
 }
--- credit-card-web/src/main/java/com/zkname/credit/card/dao/CcardJobDAO.java
+++ credit-card-web/src/main/java/com/zkname/credit/card/dao/CcardJobDAO.java
@@ -16,7 +16,7 @@ public class CcardJobDAO extends BaseDAO<CcardJob> {
 	 * @return
 	 */
 	public List<Map<String,Object>> findUserJob(long userId,Date b,Date e){
-		String sql="SELECT a.id,CONCAT(c.name,'-',b.name,'[',a.money,'元]') as title,date_format(a.jobDate,'%Y-%m-%d') as start,a.status as status FROM c_card_job as a ,c_card_info as b,c_bank as c where a.creatorId=? and a.jobDate>=? and a.jobDate<? and c.id=a.bankId and b.id=a.cardInfoId order by a.jobDate asc,a.status desc";
+		String sql="SELECT a.id,CONCAT(c.name,'-',b.name,'[',a.money,'元]',',手续[',a.feeValue,'元]') as title,date_format(a.jobDate,'%Y-%m-%d') as start,a.status as status,a.money as money,a.feeValue as fee FROM c_card_job as a ,c_card_info as b,c_bank as c where a.creatorId=? and a.jobDate>=? and a.jobDate<? and c.id=a.bankId and b.id=a.cardInfoId order by a.jobDate asc,a.status desc";
 		return super.getJdbcTemplate().queryForList(sql, userId,b,e);
 	}
 }
